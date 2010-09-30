eb7270646e93394dbd9cdd8d34017576ba246974
Leif <leif.a.andersen@gmail.com>
2010-09-29 22:14:46 -0600

    Added the needed boilerplate code.

--- src/net/leifandersen/mobile/android/netcatch/model/EpisodesDatabase.java
+++ src/net/leifandersen/mobile/android/netcatch/model/EpisodesDatabase.java
@@ -1,5 +1,33 @@
 package net.leifandersen.mobile.android.netcatch.model;
 
-public class EpisodesDatabase {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+
+/**
+ * 
+ * @author leif
+ *
+ */
+public class EpisodesDatabase extends SQLiteOpenHelper {
+
+	public EpisodesDatabase(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/net/leifandersen/mobile/android/netcatch/model/ShowsDatabase.java
+++ src/net/leifandersen/mobile/android/netcatch/model/ShowsDatabase.java
@@ -1,5 +1,33 @@
 package net.leifandersen.mobile.android.netcatch.model;
 
-public class ShowsDatabase {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+
+/**
+ * 
+ * @author leif
+ *
+ */
+public class ShowsDatabase extends SQLiteOpenHelper{
+
+	public ShowsDatabase(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
