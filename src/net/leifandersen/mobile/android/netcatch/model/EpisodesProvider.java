8f144744890b18e199a7cfb49a817e8692f0d1e3
Leif <leif.a.andersen@gmail.com>
2010-09-30 06:48:18 -0600

    Changed class structor, more boilerplate too.
    THe Providers are now actually providers and use
    the database helpers internally.

--- src/net/leifandersen/mobile/android/netcatch/model/EpisodesProvider.java
+++ src/net/leifandersen/mobile/android/netcatch/model/EpisodesProvider.java
@@ -1,34 +1,84 @@
 package net.leifandersen.mobile.android.netcatch.model;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.net.Uri;
 
 /**
  * 
  * @author leif
  *
  */
-public class EpisodesProvider extends SQLiteOpenHelper {
-
+public class EpisodesProvider extends ContentProvider {
+	
     private static final String DATABASE_NAME = "shows.db";
     private static final int DATABASE_VERSION = 2;
 	
-	public EpisodesProvider(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	/**
+	 * 
+	 * @author leif
+	 *
+	 */
+	private class DatabaseHelper extends SQLiteOpenHelper {
+	
+		public DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+	
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// TODO Auto-generated method stub
+			
+		}
+	
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO Auto-generated method stub
+			
+		}
+	
 	}
 
 	@Override
-	public void onCreate(SQLiteDatabase db) {
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		// TODO Auto-generated method stub
-		
+		return 0;
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	public String getType(Uri uri) {
 		// TODO Auto-generated method stub
-		
+		return null;
 	}
 
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCreate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
--- src/net/leifandersen/mobile/android/netcatch/model/ShowsProvider.java
+++ src/net/leifandersen/mobile/android/netcatch/model/ShowsProvider.java
@@ -1,39 +1,92 @@
 package net.leifandersen.mobile.android.netcatch.model;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.net.Uri;
+import android.util.Log;
 
 /**
  * 
  * @author leif
  *
  */
-public class ShowsProvider extends SQLiteOpenHelper{
-
+public class ShowsProvider extends ContentProvider {
+	
+	private static final String TAG = "ShowsProvider";
+	
     private static final String DATABASE_NAME = "shows.db";
     private static final int DATABASE_VERSION = 2;
     private static final String SHOWS_TABLE_NAME = "shows";
-    
-	public ShowsProvider(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	
+    /**
+     * 
+     * @author leif
+     *
+     */
+	private class DatabaseHelper extends SQLiteOpenHelper {
+	    
+		public DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+	
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL("CREATE TABLE " + SHOWS_TABLE_NAME + " ("
+						+ Shows._ID + " INTEGER PRIMARY KEY,"
+						+ Shows.TITLE + " TEXT,"
+						+ Shows.AUTHOR + " TEXT,"
+						+ Shows.FEED + " Text,"
+						+ ");");
+		}
+	
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+					+ newVersion + ", which will destroy all old data");
+			db.execSQL("DROP TABLE IF EXISTS " + SHOWS_TABLE_NAME);
+			onCreate(db);
+		}
 	}
 
 	@Override
-	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("CREATE TABLE " + SHOWS_TABLE_NAME + " ("
-					+ Shows._ID + " INTEGER PRIMARY KEY,"
-					+ Shows.TITLE + " TEXT,"
-					+ Shows.AUTHOR + " TEXT,"
-					+ Shows.FEED + " Text,"
-					+ ");");
-	}
-
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		// TODO Auto-generated method stub
-		
+		return 0;
 	}
 
+	@Override
+	public String getType(Uri uri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCreate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
