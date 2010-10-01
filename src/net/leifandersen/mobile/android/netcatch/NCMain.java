127551ebc8b32f5404ebcfe97f3f6cb1ab5cb05b
Leif <leif.a.andersen@gmail.com>
2010-10-01 10:32:24 -0600

    Added some lifetime calls for the main activity.
    Started some work on the main activity.  With a few APIs in place,
    I started some basic work on the main activity, not much, just enough
    to be able to test that it's working properly.

--- src/net/leifandersen/mobile/android/netcatch/NCMain.java
+++ src/net/leifandersen/mobile/android/netcatch/NCMain.java
@@ -1,13 +1,43 @@
 package net.leifandersen.mobile.android.netcatch;
 
+import net.leifandersen.mobile.android.netcatch.model.NetCatcher;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class NCMain extends Activity {
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	
+	NetCatcher mNC;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		mNC = new NetCatcher();
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	public void onRestart() {
+		super.onRestart();
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+	}
 }
