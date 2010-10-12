84763bc25e92c8c9ed9842ab1398b0de551d1403
Leif <leif.a.andersen@gmail.com>
2010-10-12 07:19:08 -0600

    Moved the main to activities folder (controller).
    A breakdown of how I'm thinking about this:
    Activities - Controller
    XML Layouts - View
    Content Providers - Model
    I'll probably need services and recievers, which will likely
    also fit into the controller technically, but will get their
    own packages.

---
+++ src/net/leifandersen/mobile/android/netcatch/activities/NCMain.java
@@ -0,0 +1,40 @@
+package net.leifandersen.mobile.android.netcatch.activities;
+
+import net.leifandersen.mobile.android.netcatch.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NCMain extends Activity {
+    /** Called when the activity is first created. */
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	public void onRestart() {
+		super.onRestart();
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+	}
+}
