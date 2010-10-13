7dc96050efdd31633b13bdd123ba752995e132c8
Leif <leif.a.andersen@gmail.com>
2010-10-13 14:29:46 -0600

    Added helper structs for passing data.
    These structures aren't persistant, only a convienince when passing
    data between other classes and methods.

---
+++ src/net/leifandersen/mobile/android/netcatch/providers/Episode.java
@@ -0,0 +1,73 @@
+package net.leifandersen.mobile.android.netcatch.providers;
+
+
+/**
+ * A structure to hold an episode object designed only for packing data to pass
+ * in between methods
+ * 
+ * @author Leif Andersen
+ *
+ */
+public class Episode {
+	
+	private String title;
+	private String author;
+	private String description;
+	private String media;
+	
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+	
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * @param media the media to set
+	 */
+	public void setMedia(String media) {
+		this.media = media;
+	}
+	
+	/**
+	 * @return the media
+	 */
+	public String getMedia() {
+		return media;
+	}
+}
---
+++ src/net/leifandersen/mobile/android/netcatch/providers/Show.java
@@ -0,0 +1,88 @@
+package net.leifandersen.mobile.android.netcatch.providers;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * A structure to hold a show object designed only for packing data to pass
+ * in between methods
+ * 
+ * @author Leif Andersen
+ *
+ */
+public class Show {
+	private String title;
+	private String author;
+	private String feed;
+	private Drawable image;
+	private boolean isPlayed;
+	
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+	
+	/**
+	 * @param feed the feed to set
+	 */
+	public void setFeed(String feed) {
+		this.feed = feed;
+	}
+	
+	/**
+	 * @return the feed
+	 */
+	public String getFeed() {
+		return feed;
+	}
+	
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(Drawable image) {
+		this.image = image;
+	}
+	
+	/**
+	 * @return the image
+	 */
+	public Drawable getImage() {
+		return image;
+	}
+	
+	/**
+	 * @param isPlayed the isPlayed to set
+	 */
+	public void setPlayed(boolean isPlayed) {
+		this.isPlayed = isPlayed;
+	}
+	
+	/**
+	 * @return the isPlayed
+	 */
+	public boolean isPlayed() {
+		return isPlayed;
+	}
+}
