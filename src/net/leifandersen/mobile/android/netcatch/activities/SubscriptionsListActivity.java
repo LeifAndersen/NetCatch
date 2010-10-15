2bed247a2d65c0dc80be5a8a1f66107ab3949be9
Leif <leif.a.andersen@gmail.com>
2010-10-15 10:42:59 -0600

    Added a dialog for subscribing.
    The dialog doesn't really do anything, but there is a text field
    for the user to enter the feed.  And if they click on the cancel button,
    they are unsubscribed from it.

--- src/net/leifandersen/mobile/android/netcatch/activities/SubscriptionsListActivity.java
+++ src/net/leifandersen/mobile/android/netcatch/activities/SubscriptionsListActivity.java
@@ -3,13 +3,16 @@ package net.leifandersen.mobile.android.netcatch.activities;
 import net.leifandersen.mobile.android.netcatch.R;
 import net.leifandersen.mobile.android.netcatch.providers.Show;
 import net.leifandersen.mobile.android.netcatch.providers.ShowsProvider;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -18,9 +21,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 /**
  * 
@@ -32,8 +35,11 @@ import android.widget.Toast;
  */
 public class SubscriptionsListActivity extends ListActivity {
 
-	Cursor mShows;
-	
+	private Cursor mShows;
+
+	private static final int NEW_FEED = 1;
+	private static final int NEW_FEED_DETAILS = 2;
+
 	private class ShowAdapter extends ArrayAdapter<Show> {
 		public ShowAdapter(Context context) {
 			super(context, R.layout.subscriptions_list);
@@ -48,7 +54,7 @@ public class SubscriptionsListActivity extends ListActivity {
 				row = inflater.inflate(R.layout.subscription, null);
 			else
 				row = convertView;
-			
+
 			TextView title = (TextView)row.findViewById(R.id.sc_title);
 			TextView author = (TextView)row.findViewById(R.id.sc_author);
 			ImageView image = (ImageView)row.findViewById(R.id.sc_picture);
@@ -59,7 +65,7 @@ public class SubscriptionsListActivity extends ListActivity {
 			if (d != null)
 				image.setImageDrawable(d);
 			registerForContextMenu(row);
-			
+
 			return row;
 		}
 	}
@@ -70,45 +76,73 @@ public class SubscriptionsListActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.subscriptions_list);
-		
+
 		// Set up the list
 		refreshList();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-    	MenuInflater inflator = getMenuInflater();
-    	inflator.inflate(R.layout.subscriptions_menu, menu);
+		MenuInflater inflator = getMenuInflater();
+		inflator.inflate(R.layout.subscriptions_menu, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Intent activity = new Intent();
 		switch (item.getItemId()) {
 		case R.id.sm_new_show:
-			Toast.makeText(this, "To be built", Toast.LENGTH_LONG);
-			// TODO, just a test insertion!!!
-			String newFeed = "foo";
-			
-			// TODO, actually get the feed properties!!!
-			Show show = new Show("Title", "Author", newFeed, "");
-			
-			// Add the show
-			ContentValues values = new ContentValues();
-			values.put(ShowsProvider.TITLE, show.getTitle());
-			values.put(ShowsProvider.AUTHOR, show.getAuthor());
-			values.put(ShowsProvider.FEED, show.getFeed());
-			values.put(ShowsProvider.IMAGE, show.getImagePath());
-			getContentResolver().insert(ShowsProvider.SUBSCRIPCTIONS_CONTENT_URI, values);
-			
-			// Refresh and return
-			refreshList();
+			showDialog(NEW_FEED);
 			return true;
 		}
 		return false;
 	}
-	
+
+	@Override
+	protected Dialog onCreateDialog(int id, Bundle args) {
+		Dialog dialog = null;
+		switch(id) {
+		case NEW_FEED:
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			LayoutInflater inflater = 
+				(LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+			View layout = inflater.inflate(R.layout.subscription_feed_dialog, null);
+			builder.setView(layout);
+			builder.setCancelable(false);
+			builder.setPositiveButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO, actually get the feed properties!!!
+					String newFeed = "";
+					Show show = new Show("Title", "Author", newFeed, "");
+
+					// Add the show
+					ContentValues values = new ContentValues();
+					values.put(ShowsProvider.TITLE, show.getTitle());
+					values.put(ShowsProvider.AUTHOR, show.getAuthor());
+					values.put(ShowsProvider.FEED, show.getFeed());
+					values.put(ShowsProvider.IMAGE, show.getImagePath());
+					getContentResolver().insert(ShowsProvider.SUBSCRIPCTIONS_CONTENT_URI, values);
+
+					// Refresh and return
+					refreshList();
+				}
+			});
+			builder.setNegativeButton(getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.cancel();
+				}
+			});
+			dialog = builder.create();
+			break;
+		default:
+			dialog = null;
+		}
+		return dialog;
+	}
+
 	private void refreshList() {
 		// Reset the view, 
 		adapter = new ShowAdapter(this);
@@ -129,5 +163,5 @@ public class SubscriptionsListActivity extends ListActivity {
 				adapter.add(s);
 			} while (mShows.moveToNext());
 	}
-	
+
 }
