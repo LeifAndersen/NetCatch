95b57e70c27104d6b2e6461e5f88804fab9a8fac
Leif Andersen <leif@leif-VPCEB11FM.(none)>
2010-10-18 11:01:40 -0600

    Finished the eppisode adapter, more boilerplate.

--- src/net/leifandersen/mobile/android/netcatch/activities/EpisodesListActivity.java
+++ src/net/leifandersen/mobile/android/netcatch/activities/EpisodesListActivity.java
@@ -2,31 +2,68 @@ package net.leifandersen.mobile.android.netcatch.activities;
 
 import net.leifandersen.mobile.android.netcatch.R;
 import net.leifandersen.mobile.android.netcatch.providers.Episode;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.Adapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class EpisodesListActivity extends Activity {
+public class EpisodesListActivity extends ListActivity {
 
-	Adapter mAdapter;
+	String mShowName;
 	
 	private class EpisodeAdapter extends ArrayAdapter<Episode> {
-
 		public EpisodeAdapter(Context context) {
 			super(context, R.layout.episodes_list);
 		}
-		
+
+		@Override
+		public View getView(int position, View convertView,
+				ViewGroup parent) {
+			LayoutInflater inflater = getLayoutInflater();
+			View row;
+			if(convertView == null)
+				row = inflater.inflate(R.layout.episode, null);
+			else
+				row = convertView;
+			
+			TextView title = (TextView)row.findViewById(R.id.ep_title);
+			TextView description = (TextView)row.findViewById(R.id.ep_description);
+			TextView date = (TextView)row.findViewById(R.id.ep_release_date);
+			Episode episode = getItem(position);
+			title.setText(episode.getTitle());
+			description.setText(episode.getDescription());
+			date.setText(episode.getDate());
+			
+			registerForContextMenu(row);
+			return row;
+		}
 	}
+
+	EpisodeAdapter mAdapter;
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.episodes_list);
+
+		// Get the show name
+		mShowName = ""; // TODOo
 		
 		// Set the List Adapter
+		refreshList();
+	}
+
+	private void refreshList() {
 		mAdapter = new EpisodeAdapter(this);
+		setListAdapter(mAdapter);
+		
+		// TODO Add the elements
+		
 	}
 	
 }
