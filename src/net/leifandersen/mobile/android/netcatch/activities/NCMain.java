1c783bcff280074c419be649bed564978441fd27
Leif <leif@leifandersen.net>
2010-10-31 20:57:13 -0600

    Added an actual main menu.
    Nothing is clickable yet though.

--- src/net/leifandersen/mobile/android/netcatch/activities/NCMain.java
+++ src/net/leifandersen/mobile/android/netcatch/activities/NCMain.java
@@ -2,9 +2,12 @@ package net.leifandersen.mobile.android.netcatch.activities;
 
 import net.leifandersen.mobile.android.netcatch.R;
 import android.app.ListActivity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 /**
  * 
@@ -17,14 +20,22 @@ import android.widget.ArrayAdapter;
 public class NCMain extends ListActivity {
 	/** Called when the activity is first created. */
 	
+	ArrayAdapter<String> mAdapter;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
-		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.test_list_item));
+		mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.main_menu));
+		setListAdapter(mAdapter);
 	}
 
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+	}
+	
 	@Override
 	public void onPause() {
 		super.onPause();
