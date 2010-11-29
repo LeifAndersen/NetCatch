9ecb8fb687bae6bedc3ae9c66817ca3dd3fcb3c8
Leif <leif@leifandersen.net>
2010-11-29 08:09:13 -0700

    Fixed the database creation.
    I added the fields to the wrong database.

--- src/net/leifandersen/mobile/android/netcatch/providers/ShowsProvider.java
+++ src/net/leifandersen/mobile/android/netcatch/providers/ShowsProvider.java
@@ -176,8 +176,6 @@ public class ShowsProvider extends ContentProvider {
 		+ DESCRIPTION + " TEXT," 
 		+ MEDIA + " TEXT, "
 		+ DATE + " TEXT, "
-		+ LENGTH + " INTEGER,"
-		+ POSITION + " INTEGER,"
 		+ PLAYED + " BOOLEAN" + ");";
 	
 	private static final String createTableString(final String tableName) {
@@ -188,6 +186,8 @@ public class ShowsProvider extends ContentProvider {
 		+ DESCRIPTION + " TEXT," 
 		+ MEDIA + " TEXT, "
 		+ DATE + " TEXT, "
+		+ LENGTH + " INTEGER,"
+		+ POSITION + " INTEGER,"
 		+ PLAYED + " BOOLEAN" + ");";
 	}
 	
