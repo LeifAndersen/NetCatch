39c5e85f10a070791ead801dbf3d6428d20fa0e9
Leif <leif@leifandersen.net>
2010-12-20 22:11:56 -0700

    Moved TexturedListAdapter into ShowListActivity
    as that is the only place where it should be used anyway.

--- src/net/leifandersen/mobile/android/netcatch/activities/ShowsListActivity.java
+++ src/net/leifandersen/mobile/android/netcatch/activities/ShowsListActivity.java
@@ -14,27 +14,36 @@
 package net.leifandersen.mobile.android.netcatch.activities;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import net.leifandersen.mobile.android.netcatch.R;
 import net.leifandersen.mobile.android.netcatch.providers.Show;
 import net.leifandersen.mobile.android.netcatch.providers.ShowsProvider;
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * 
@@ -46,6 +55,61 @@ import android.widget.ListView;
  */
 public class ShowsListActivity extends Activity {
 
+	public class TexturedListAdapter extends BaseAdapter {
+		private List<Show> items = new ArrayList<Show>();
+		private Context context;
+		final Typeface vera, veraBold;
+		
+		public int getCount() {
+			return items.size();
+		}
+		
+		public long getItemId(int position) {
+			return position;
+		}
+		
+		public Show getItem(int position) {
+			return items.get(position);
+		}
+		
+		public TexturedListAdapter(Context context, ArrayList<Show> shows) {
+			vera = Typeface.createFromAsset(context.getAssets(), "Vera.ttf");
+			veraBold = Typeface.createFromAsset(context.getAssets(), "VeraBd.ttf");
+			this.items = shows;
+			this.context = context;
+		}
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+			if (v == null) {
+				LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				v = vi.inflate(R.layout.main_menu_list_item_textured, null);
+			}
+			Show s = items.get(position);
+			if (s != null) {
+				
+				TextView title = (TextView)v.findViewById(R.id.list_feed_title);
+				TextView counts = (TextView)v.findViewById(R.id.list_feed_counts);
+				TextView updateDate = (TextView)v.findViewById(R.id.list_feed_update_time);
+				ImageView art = (ImageView)v.findViewById(R.id.list_album_art);
+				
+					//placeholders until Show class description is finalized
+					title.setText(s.getTitle());
+					title.setTypeface(veraBold);
+					
+					counts.setText(s.getDescription());
+					counts.setTypeface(vera);
+					
+					updateDate.setText(s.getFeed());
+					updateDate.setTypeface(vera);
+				
+				if(s.getImage() == null) art.setImageResource(R.drawable.image_album_background);
+				else art.setImageDrawable(s.getImage());
+			}
+			return v;
+		}
+	}
+	
 	private static final int NEW_FEED = 1;
 	private LinearLayout background;
 	private FrameLayout header;
